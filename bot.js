9974fb1860dd42f20716f541459c2316c8669aee
TheProgrammingRaccoon <35145859+graziel666@users.noreply.github>
2022-09-13 20:33:26 -0300

    more cleaning and fix some bugs

--- bot.js
+++ bot.js
@@ -36,14 +36,14 @@ client.on('interactionCreate', async interaction => {
     // Add components to modal
 
     // Create the text input components
-    const favoriteColorInput = new TextInputBuilder()
+    const userToReport = new TextInputBuilder()
       .setCustomId('Report')
       // The label is the prompt the user sees for this input
       .setLabel("Ingame name or user")
       // Short means only a single line of text
       .setStyle(TextInputStyle.Short);
 
-    const hobbiesInput = new TextInputBuilder()
+    const reasonToReport = new TextInputBuilder()
       .setCustomId('Reason')
       .setLabel("What's the reason you wanna report them?")
       // Paragraph means multiple lines of text.
@@ -51,8 +51,8 @@ client.on('interactionCreate', async interaction => {
 
     // An action row only holds one text input,
     // so you need one action row per text input.
-    const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
-    const secondActionRow = new ActionRowBuilder().addComponents(hobbiesInput);
+    const firstActionRow = new ActionRowBuilder().addComponents(userToReport);
+    const secondActionRow = new ActionRowBuilder().addComponents(reasonToReport);
 
     // Add inputs to the modal
     modal.addComponents(firstActionRow, secondActionRow);
@@ -60,37 +60,6 @@ client.on('interactionCreate', async interaction => {
     // Show the modal to the user
     await interaction.showModal(modal);
 
-
-    client.on('interactionCreate', interaction => {
-        if (interaction.type !== InteractionType.ModalSubmit) return;
-
-        //getting user
-        const userID = interaction.user.id;
-
-        // Get the data entered by the user
-        const User_to_report = interaction.fields.getTextInputValue('Report');
-        const Reason_of_report = interaction.fields.getTextInputValue('Reason');
-        //console.log({ User_to_report, Reason_of_report });
-
-        //building message
-
-        //leaving this here in case we wanna make it 100% annonymous, even for us
-        // const reported_user = `User: ${User_to_report}`;
-
-        //with name
-        const reported_user = `<@${userID}> has reported ${User_to_report}`;
-
-        const report_reason = `Reason: ${Reason_of_report}`;
-//testy-test 1018573624606736445
-//squishee 1018673990786613358
-        const channel = client.channels.cache.get('1018573624606736445');
-        channel.send(reported_user);
-        channel.send(report_reason);
-
-        if (interaction.customId === 'myModal') {
-            interaction.reply({ content: 'Your submission was received successfully!', ephemeral: true });
-        }
-    });
   }
   if (interaction.commandName === 'cookie') {
 
@@ -109,6 +78,38 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
+client.on('interactionCreate', async interaction => {
+  if (interaction.type !== InteractionType.ModalSubmit) return;
 
+
+  if (interaction.customId === 'myModal') {
+  //getting user
+  const userID = interaction.user.id;
+
+  // Get the data entered by the user
+  const User_to_report = interaction.fields.getTextInputValue('Report');
+  const Reason_of_report = interaction.fields.getTextInputValue('Reason');
+  //	console.log({ User_to_report, Reason_of_report });
+
+  //building message
+
+  //leaving this here in case we wanna make it 100% annonymous, even for us
+  // const reported_user = `User: ${User_to_report}`;
+
+  //with name
+  const reported_user = `<@${userID}> has reported ${User_to_report}`;
+
+  const report_reason = `Reason: ${Reason_of_report}`;
+
+
+    //sneakies 1018223694922907808
+    //squishee 1018673990786613358
+  const channel = client.channels.cache.get('1018673990786613358');
+  channel.send(reported_user);
+  channel.send(report_reason);
+
+    await interaction.reply({ content: 'Your submission was received successfully!', ephemeral: true });
+  }
+});
 
 client.login(token);
